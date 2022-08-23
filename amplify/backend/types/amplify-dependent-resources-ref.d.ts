865f452a095a48b13ed49629a136ea0ddcb71109
evan kirkiles <kirkilese@gmail.com>
2022-08-23 15:04:39 +0200

    Added amplify, removed terraform.

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "papercraftclub783d8fae": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "papercraftclub": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
---
+++ src/pages/_document.tsx
@@ -0,0 +1,44 @@
+/*
+ * _document.tsx
+ * author: evan kirkiles
+ * created on Tue Aug 23 2022
+ * 2022 papercraft club
+ */
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Kosugi+Maru:wght@400;700&display=optional"
+            rel="stylesheet"
+          />
+          <link rel="icon" href="/favicon.ico" />
+          <meta name="title" content="papercraft club"></meta>
+          <meta name="description" content="big brains in little bodies, from an art space with cyber qualities. bridging the gap between the digital world and the physical world, one byte at a time."></meta>
+          <meta name="keywords" content="art, artist, painting, robots, tech, robots, automation, visuals, graphic design, blender, 3d, 2d, concept, sculpture, game, video, "></meta>
+          <meta name="robots" content="index, follow"></meta>
+          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8"></meta>
+          <meta name="language" content="en-us"></meta>
+          <meta name="author" content="evan kirkiles"></meta>
+          <meta name="apple-mobile-web-app-title" content="papercraft club" />
+          <meta name="application-name" content="papercraft club" />
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          {/* <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ffffff" /> */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument;
