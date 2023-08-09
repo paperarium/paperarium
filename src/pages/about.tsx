2fb14d2e48087f44e0b882d89a761a787a827de2
Sam Kirkiles <sam.kirkiles@gmail.com>
2023-08-09 11:55:06 -0400

    Add timeline

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,6 +2,12 @@ import type { NextPage } from 'next';
 import { NextSeo } from 'next-seo';
 import Head from 'next/head';
 import s from '../styles/About.module.scss';
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
 
 const AboutPage: NextPage = () => {
   return (
@@ -14,7 +20,47 @@ const AboutPage: NextPage = () => {
         />
       </Head>
       <div className={s.login_page_container}>
-        <h1>The History of Papercrafting</h1>
+        <h1>The History of Papercraftding</h1>
+        <div style={{ background: 'red' }}>
+          <div>Nintendo papercraft released</div>
+          <div>Haywan Lifesiez Link</div>
+          <div>Char Ganondorf</div>
+          <div>Nintendo papercrat contest</div>
+          <div>Ninjatoes</div>
+          <Timeline position="left">
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot />
+                <TimelineConnector />
+              </TimelineSeparator>
+              <TimelineContent>
+                <div>Pepakura released</div>
+              </TimelineContent>
+            </TimelineItem>
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot />
+                <TimelineConnector />
+              </TimelineSeparator>
+              <TimelineContent>
+                <div style={{ background: 'blue' }}>Paperpokes</div>
+              </TimelineContent>
+            </TimelineItem>
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot />
+                <TimelineConnector />
+              </TimelineSeparator>
+              <TimelineContent>Sleep</TimelineContent>
+            </TimelineItem>
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot />
+              </TimelineSeparator>
+              <TimelineContent>Repeat</TimelineContent>
+            </TimelineItem>
+          </Timeline>
+        </div>
       </div>
     </>
   );
